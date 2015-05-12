130ee423834962996e35f64ad5223da63599fe94
hamed <hmdhasani@hotmail.com>
2015-05-12 11:48:46 +0430

    add motion response with ok

--- app/controllers/motions_controller.rb
+++ app/controllers/motions_controller.rb
@@ -6,7 +6,8 @@ class MotionsController < ApplicationController
 
   def remote_motion
     @motion = Motion.create(:lock_id=>params[:lock_id])
-    redirect_to action: "index"
+    render :layout =>false
+    #redirect_to action: "index"
   end
 
   def index
